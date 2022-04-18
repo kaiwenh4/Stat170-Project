743240e911486d6bad1bf7ebed3b50f8fbc453b5
kaiwenh4 <74094966+kaiwenh4@users.noreply.github.com>
2022-04-17 19:33:43 -0700

    update more attr

--- feature engineering/shop_user_avg.sql
+++ feature engineering/shop_user_avg.sql
@@ -1,13 +1,29 @@
 set search_path to yelp_data;
 
-CREATE INDEX bid ON review(business_id);
-CREATE INDEX bid ON shop_info(business_id);
+CREATE INDEX re_bid ON review(business_id);
+CREATE INDEX shop_bid ON shop_info(business_id);
 CREATE INDEX uid ON user_info(user_id);
 
 CREATE VIEW user_aver_for_shop AS
-SELECT t1.business_id, avg(ui.average_stars)
+SELECT t1.business_id
+	,avg(ui.average_stars) as avg_stars
+	,avg(ui.review_count) as avg_review_count
+	,avg(ui.useful) as avg_useful
+	,avg(ui.funny) as avg_funny
+	,avg(ui.cool) as avg_cool
+	,avg(fans) as avg_fans
+	,avg(compliment_hot) as avg_compliment_hot
+	,avg(compliment_more) as avg_compliment_more
+	,avg(compliment_profile) as avg_compliment_profile
+	,avg(compliment_cute) as avg_compliment_cute
+	,avg(compliment_list) as avg_compliment_list
+	,avg(compliment_note) as avg_compliment_note
+	,avg(compliment_plain) as avg_compliment_plain
+	,avg(compliment_cool) as avg_compliment_cool
+	,avg(compliment_funny) as avg_compliment_funny
+	,avg(compliment_writer) as avg_compliment_writer
+	,avg(compliment_photos) as avg_compliment_photos
 FROM (SELECT re.business_id, re.user_id
 	 FROM review re join shop_info si on re.business_id = si.business_id) t1 
 	 join user_info ui on t1.user_id = ui.user_id
 GROUP BY 1;
-
