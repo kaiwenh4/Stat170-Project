33a9c2880233431c39bdb1aa825beb6478e86b1f
kaiwenh4 <74094966+kaiwenh4@users.noreply.github.com>
2022-05-07 21:31:18 -0700

    update

---
+++ feature engineering/shop_review_avg.sql
@@ -0,0 +1,11 @@
+set search_path to yelp_data;
+
+CREATE VIEW avg_review AS
+
+SELECT business_id
+    ,avg(useful) as avg_useful
+    ,avg(funny) as avg_funny
+    ,avg(cool) as avg_cool
+FROM yelp_review re join yelp_coffee_info ci
+        on re.business_id = ci.business_id
+GROUP BY 1;
--- feature engineering/shop_user_avg.sql
+++ feature engineering/shop_user_avg.sql
@@ -1,8 +1,8 @@
 set search_path to yelp_data;
 
-CREATE INDEX re_bid ON review(business_id);
-CREATE INDEX shop_bid ON shop_info(business_id);
-CREATE INDEX uid ON user_info(user_id);
+CREATE INDEX re_bid ON yelp_review(business_id);
+CREATE INDEX shop_bid ON yelp_coffee_info(business_id);
+CREATE INDEX uid ON yelp_user_info(user_id);
 
 CREATE VIEW user_aver_for_shop AS
 SELECT t1.business_id
@@ -23,7 +23,9 @@ SELECT t1.business_id
 	,avg(compliment_funny) as avg_compliment_funny
 	,avg(compliment_writer) as avg_compliment_writer
 	,avg(compliment_photos) as avg_compliment_photos
+	,avg(LOCALTIMESTAMP - correct_yelp_since) as yelp_years
+	,avg(combined_compliment) as avg_total_compliment
 FROM (SELECT re.business_id, re.user_id
-	 FROM review re join shop_info si on re.business_id = si.business_id) t1 
-	 join user_info ui on t1.user_id = ui.user_id
+	 FROM yelp_review re join yelp_shop_info si on re.business_id = si.business_id) t1 
+	 join yelp_user_info ui on t1.user_id = ui.user_id
 GROUP BY 1;
